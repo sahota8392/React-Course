f1b82f1055bff0368e1cddaa371332d6c52ff5a8
sahota8392 <sahota8392@gmail.com>
2022-01-27 20:52:15 -0800

    Adding to the Single Page App

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import {Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 function Home(props) {
     return(
         <div className="container">     {/* boostrap class of Container */}
+            <div className="row">
+                <div className="col-md m-1">
+                    <RenderCard item={props.campsite} />
+                </div>
+
+                <div className="col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+
+                <div className="col-md m-1">
+                    <RenderCard item={props.partner} />
+                </div>
+            </div>
             <h4>Home</h4>
         </div>
-    )
+    );
+}
+
+//the function RenderCard will be called in the function Home above
+function RenderCard({item}) {
+    return(
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
 }
 
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,23 +4,33 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { CAMPSITES } from '../shared/campsites';
-import Home from'./HomeComponent';                                  //importing Home Componenet from HomeComponent.js
+import { CAMPSITES } from '../shared/campsites';                     //importing a module
+import Home from './HomeComponent';                                  //importing Home Componenet from HomeComponent.js
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';         //so when a link is clicked, we can use these to reroute users
-
+import { COMMENTS } from '../shared/comments';                      //'../' is same as using cd.. in terminal
+import { PARTNERS } from '../shared/partners';                      //update the class constructor to add these
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,                                   //adding the importing array here
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
     render() {
         const HomePage = () => {                //defined in maincomponent so only accessible here - locally scoped
         return (
-            <Home />
+            <Home 
+                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}    //Filtering for objects where featured propery evaluates as true
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}  //filter returns an array so we need to pass an object, using [0] to access first object in array
+                partner={this.state.partners.filter(partner => partner.featured)[0]}
+            />
         );
     };
     
@@ -30,7 +40,9 @@ class Main extends Component {
             <Switch>
                 <Route path='/home' component = {HomePage} />
                 <Route path='/directory' render = {() => <Directory campsites = {this.state.campsites} />} /> 
-                <Redirect to='/home' />
+                <Route exact path='/contactus' component={Contact} />       
+                <Redirect to='/home' />                                {/* Redirect must the last one */}
+                
             </Switch>
             <Footer />
         </div>
